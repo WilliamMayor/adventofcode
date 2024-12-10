235b9edad2999709f836d3e741bacf22ed5a521e
William Mayor <mail@mayortech.co.uk>
2024-12-10 01:07:04 +0000

    Add docs for q9

--- 2024/q9/p1.py
+++ 2024/q9/p1.py
@@ -1,3 +1,48 @@
+"""
+Understanding
+-------------
+
+Rearrange individual file blocks on a harddrive to pack the blocks as close
+together as possible (with as little free space between them).
+
+Input is an alternatice sequence of file size and then free space.
+
+e.g. 12345 means a file made up of one block, followed by 2 blocks of free space
+followed by a file of 3 blocks, then 4 blocks free space, then a 5 block file.
+
+Starting from the right hand side move blocks to the leftmost available free
+space.
+
+The answer is a "checksum" involving the file IDs (the file's original location)
+and the block indecies that the file is located on.
+
+Notes
+-----
+
+I was busy today, so I couldn't work on this until the evening after it was
+published, resulting in a ranking of:
+
+Day 9 - Time 18:06:52 - Rank 44934 - Score 0
+
+I tried to make this one as efficient as I could, and I think it's pretty fast.
+It takes 0.03s on my machine.
+
+The general idea is to consume from both ends of the input at the same time,
+moving blocks to the left. Stopping when you reach some kind of middle point.
+
+When you're moving from left to right and you encounter a file, you can
+calculate the checksum straightaway because you know you're not going to move a
+file that you've processed from the left hand side.
+
+When you're moving from right to left and you move some blocks around you can
+calculate the checksum of the repositioned blocks because you know you won't
+move them a second time.
+
+There's a log of tracking of index positions, which feels a bit like code smell.
+I'd normally try to avoid using the enumerate function as much as I have here.
+"""
+
+
 def main():
     with open("q9/input.txt", "r") as fd:
         input_ = list(map(int, fd.read().strip()))
--- 2024/q9/p2.py
+++ 2024/q9/p2.py
@@ -1,3 +1,42 @@
+"""
+Understanding
+-------------
+
+Building on Part 1, instead of moving individual blocks around, you now have to
+move whole files from the right to the left, if they'll fit.
+
+You only try to move the file once, even if subsequent file relocations have
+opened up enough free space to move the file in a second pass.
+
+Notes
+-----
+
+Part 2 took me about 1h20m after part 1:
+
+Day 9 - Time 19:44:24 - Rank 35206 - Score 0
+
+This one doesn't feel as efficient as Part 1, lots more loops. But it still runs
+in 1.05s on my machine, so I'm happy.
+
+The approach I took was to do a first pass over the input finding all the free
+space slots (and their sizes). Then move backwards through the input adding
+files to those free slots where they would fit. When you move a file, calculate
+its checksum.
+
+Finally, do a third pass in order to calculate the checksum for any files that
+didn't get moved around.
+
+I quickly spotted that moving a small file into a large free space would still
+leave free space available. So you have to track the remaining space and the
+remaining free locations.
+
+The part I missed, the part I probably spent most of the hour trying to fix, was
+that when you loop through the free space from left to right, you should stop
+when the free space is to the right of the file you're considering. Obvious now
+I know the answer, but that's what I missed this time around.
+"""
+
+
 def main():
     with open("q9/input.txt", "r") as fd:
         input_ = list(map(int, fd.read().strip()))
@@ -43,8 +82,6 @@ def main():
             checksum += (map_idx // 2) * sum(range(b_idx, b_idx + file_len))
 
     print(checksum)
-
-    print("Hoping for less than 6848626407700")
 
 
 if __name__ == "__main__":
